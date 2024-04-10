a10fc72787bec3f5383108322dec852ac7589263
PedroAugustoRamalhoDuarte <pedroaugustorduarte@gmail.com>
2024-04-10 08:57:54 -0300

    :tada: Initialize gem

---
+++ spec/avo/api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Avo::Api do
+  it "has a version number" do
+    expect(Avo::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "avo/api"
+
+ENV["RAILS_ENV"] = "test"
+
+require_relative "../spec/dummy/config/environment"
+
+ENV["RAILS_ROOT"] ||= "#{File.dirname(__FILE__)}../../../spec/dummy"
+
+require "rspec/rails"
+require "database_cleaner"
+
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
+
+
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before do
+    DatabaseCleaner.start
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
+
+  config.after(:suite) do
+    FileUtils.rm_rf(File.expand_path("../test.sqlite3", __FILE__))
+  end
+end
